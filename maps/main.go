7de7d4eab8c77ac0260408a1ab2963c5d8f86d35
vndev <bhvinh@outlook.com.vn>
2023-03-06 17:34:12 +0700

    Complete maps

--- maps/main.go
+++ maps/main.go
@@ -1,1 +1,18 @@
-package maps
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4b745",
+		"white": "#ffffff",
+	}
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
